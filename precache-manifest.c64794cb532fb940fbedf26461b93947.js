e96af55478af35b193429b109e0220d8d39015a9
Sakthi7081 <Sakthivel434@gmail.com>
2020-04-17 21:32:04 +0530

    Updates

---
+++ precache-manifest.c64794cb532fb940fbedf26461b93947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce90cc41fff50f6cd709932f85ff9a3b",
+    "url": "/maintainer/index.html"
+  },
+  {
+    "revision": "e7a4c36e971bfc24d69f",
+    "url": "/maintainer/static/js/2.3fdfebde.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/maintainer/static/js/2.3fdfebde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa4928ffd489b1cffb0",
+    "url": "/maintainer/static/js/main.bab06099.chunk.js"
+  },
+  {
+    "revision": "5e096bb996bfaa3dc56c",
+    "url": "/maintainer/static/js/runtime-main.969f2574.js"
+  }
+]);
